f469f3021a6594ca3188af75c0154d2a24ea0a87
OSR <osrsla@gmail.com>
2023-09-03 18:10:15 +0900

    feat: Add shakey bell

--- Bankey/Login/LoginViewController.swift
+++ Bankey/Login/LoginViewController.swift
@@ -152,12 +152,12 @@ extension LoginViewController {
             return
         }
 
-        if username.isEmpty || password.isEmpty {
-            configureView(withMessage: "Username / password cannot be blank")
-            return
-        }
+//        if username.isEmpty || password.isEmpty {
+//            configureView(withMessage: "Username / password cannot be blank")
+//            return
+//        }
             
-        if username == "Flynn", password == "Welcome" {
+        if username == "", password == "" {
             signInButton.configuration?.showsActivityIndicator = true
             delegate?.didLogin()
         } else {
